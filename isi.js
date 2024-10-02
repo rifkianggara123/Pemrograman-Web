cad1e4c676d6b0c8064d0cfaacb2b12cc339996e
rifkianggara123 <rif1234589999@gmail.com>
2024-10-02 14:09:31 +0700

    isii

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=skyblue>");
+document.writeln("<H1>UNIVERSITAS LAMPUNG</H1>);
+document.writeln("<H2>PENDIDIKAN TEKNOLOGI INFORMASI</H2>");
+document.writeln("<H3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGATAHUAN ALAM</H3>");
+document.writeln("<hr width=600 cplpr=skyblue>");
+document.writeln("</center>");
