b7cb16d8cad780b01285b5fc1ec31e59f690ace2
rifkianggara123 <rif1234589999@gmail.com>
2024-11-26 23:00:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.getElementById('menu-bars').addEventListener('click', () => {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 });
+
+// Initialize Swiper.js
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000, // Delay 3 detik untuk auto slide
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
